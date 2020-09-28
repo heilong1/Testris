c8c19457707a59ccc48d0778a4a294292452a1f6
heilong1 <68329605+heilong1@users.noreply.github.com>
2020-09-28 19:19:52 +0300

    ImageCreating step one

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import TetTable from './game/table'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="gwin">
+      
+      <TetTable />
     </div>
   );
 }
 
 export default App;
+
+
+// Попытка реализовать тетрис в более менее классической механике.
+// Концепция.
+// Двумерная матрица представлена стейтом и рендерится во все места.
+// Фигуры объекты, непонятно как будут синхронизироваться с матрицей.
+
+// Поле 10х20, двумерный массив. Нарисовать сразу.
+// Каждый элемент массива будет чем-нибудь этим самым.
---
+++ src/game/table.js
@@ -0,0 +1,25 @@
+import React, {useState} from 'react';
+
+export default function TetTable() {
+
+  const matrix = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+  ]
+
+  const [tMatrix, setTMatrix] = useState([])
+
+  return (
+      <div className="table">
+
+      </div>
+  )
+}
